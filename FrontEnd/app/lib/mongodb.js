87ebe69cde59d87ee266312fb427b9a88ce609f4
Abdi3214 <raboy454@gmail.com>
2025-07-12 19:00:02 +0300

    Delete FrontEnd directory

--- FrontEnd/app/lib/mongodb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
-  return mongoose.connect("mongodb://localhost:27017/mydb");
-};
-
-export default connectDB;
